06d74cb086c90614b6bfcfa83b100ae96d116749
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-03-30 20:09:58 -0300

    1005 Average 1
    https://www.urionlinejudge.com.br/judge/en/problems/view/1005

---
+++ 1 - Beginner/1005 Average 1/C/Main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+float a,b;
+scanf("%f", &a);
+scanf("%f", &b);
+a*= 3.5;
+b*=7.5;
+a+= b;
+printf("MEDIA = %.5f \n",a/11);
+}
+
+
---
+++ 1 - Beginner/1005 Average 1/Python/main.py
@@ -0,0 +1,3 @@
+a = float(input(""))*3.5
+a += float(input(""))*7.5
+print(round(a/11, 5))
