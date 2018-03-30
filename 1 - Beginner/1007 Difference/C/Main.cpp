10378f0c496e7b3c45ace3740f62fe32e0c1fb8b
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-03-30 20:25:25 -0300

    1007 Difference
    https://www.urionlinejudge.com.br/judge/en/problems/view/1007

---
+++ 1 - Beginner/1007 Difference/C/Main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+int a,b,c,d;
+scanf("%d", &a);
+scanf("%d", &b);
+scanf("%d", &c);
+scanf("%d", &d);
+a = ((a*b)-(c*d));
+printf("DIFERENCA = %d",a);
+}
+
+
---
+++ 1 - Beginner/1007 Difference/Python/main.py
@@ -0,0 +1,6 @@
+a= int(input(""))
+b= int(input(""))
+a= a*b
+b = int(input(""))
+b *= int(input(""))
+print("DIFERENCA = "+str(a-b))
