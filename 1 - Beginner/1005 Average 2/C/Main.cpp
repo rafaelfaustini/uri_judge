fb1da49272e28986c11cfa7e9427422504a334e3
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-03-30 20:17:35 -0300

    1006 Average 2
    Adicionado detalhe de string no Average 1 e feito Average 2
    https://www.urionlinejudge.com.br/judge/en/problems/view/1006

---
+++ 1 - Beginner/1005 Average 2/C/Main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+float a,b,c;
+scanf("%f", &a);
+scanf("%f", &b);
+scanf("%f", &c);
+a*= 2;
+b*=3;
+c*=5;
+a+= b+c;
+printf("MEDIA = %.1f \n",a/10);
+}
+
+
---
+++ 1 - Beginner/1005 Average 2/Python/main.py
@@ -0,0 +1,4 @@
+a = float(input(""))*2
+a += float(input(""))*3
+a += float(input(""))*5
+print("MEDIA = "+str(round(a/10, 1)))
