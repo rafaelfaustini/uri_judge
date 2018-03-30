45b6605650f4a3e93670448f1204444f7b2c39c6
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-03-30 18:24:43 -0300

    1001 Extremely Basic
    Feito em C e python
    https://www.urionlinejudge.com.br/judge/en/problems/view/1001

---
+++ 1 - Beginner/1001 Extremely Basic/C/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+	int n1,n2;
+	scanf("%d",&n1);
+	scanf("%d",&n2);
+	n1 += n2;
+	printf("x = %d",n1);	
+}
---
+++ 1 - Beginner/1001 Extremely Basic/Python/main.py
@@ -0,0 +1,3 @@
+n1 = int(input("Qual o número 1: "));
+n1 += int(input("Qual o numero 2: "));
+print(n1)
