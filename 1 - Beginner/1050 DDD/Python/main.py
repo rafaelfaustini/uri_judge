9d70a6e82d8d8798e9c3e70e1bf34eab9926bde2
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-03-31 11:09:11 -0300

    1050 DDD
    https://www.urionlinejudge.com.br/judge/en/problems/view/1050

---
+++ 1 - Beginner/1050 DDD/Python/main.py
@@ -0,0 +1,5 @@
+a= int(input(""))
+possibilidades = {61:'Brasilia', 71:'Salvador', 11:'Salvador',21:'Rio de Janeiro', 32:'Juiz de Fora', 19:'Campinas', 27:'Vitoria', 31:'Belo Horizonte'}
+print(possibilidades.get(a, 'DDD não cadastrado'))
+
+
