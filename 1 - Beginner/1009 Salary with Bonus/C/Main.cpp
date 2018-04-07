ee595b5a32dd07b73ddc7b0edf58bd14b041407f
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-04-06 22:12:16 -0300

    1009 Salary with Bonus
    https://www.urionlinejudge.com.br/judge/en/problems/view/1009

---
+++ 1 - Beginner/1009 Salary with Bonus/C/Main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+char name[50];
+double salary;
+double total;
+scanf("%s", &name);
+scanf("%lf", &salary);
+scanf("%lf", &total);
+total *= 0.15;
+printf("TOTAL = R$ %.2f \n",salary+total);
+}
+
+
---
+++ 1 - Beginner/1009 Salary with Bonus/Python/main.py
@@ -0,0 +1,6 @@
+name= str(input(""))
+salary= float(input(""))
+total= float(input(""))
+total*=0.15
+print("TOTAL = R$ "+str(round(total+salary,2)));
+
