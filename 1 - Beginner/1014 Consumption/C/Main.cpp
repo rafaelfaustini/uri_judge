43d13f789fc114272f3814a266d69c393ba82679
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-04-07 14:15:26 -0300

    1014 Consumption
    https://www.urionlinejudge.com.br/judge/en/problems/view/1014

---
+++ 1 - Beginner/1014 Consumption/C/Main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+int x;
+scanf("%d",&x);
+float y;
+scanf("%f",&y);
+printf("%.3f km/l", x/y);
+}
+
+
---
+++ 1 - Beginner/1014 Consumption/Python/main.py
@@ -0,0 +1,4 @@
+x = int(input(""))
+y = float(input(""))
+print(str(round(x/y,3))+" km/l")
+    
