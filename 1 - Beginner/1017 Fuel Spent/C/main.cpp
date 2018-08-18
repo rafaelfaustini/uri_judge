2e8e915da7f1b9eaf1b1e3cc704597f4bcef4172
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-08-18 16:47:54 -0300

    1017

--- 1 - Beginner/1017 Fuel Spent/C/main.cpp
+++ 1 - Beginner/1017 Fuel Spent/C/main.cpp
@@ -1,15 +1,74 @@
 #include <stdio.h>
 
+int use(int bank[], int value){
+	int i;
+	for(i=0;i<7;i++){
+		//printf("Valor de i %d | elemento: %d\n",i,bank[i]);
+		if(value >= bank[i]){
+			return value-bank[i];
+		}
+	}
+	return value;
+}
+
+void print_bank(int used[],int bank[]){
+int i;
+for(i=0;i<7;i++){
+	printf("%d nota(s) de R$ %d,00\n",used[i],bank[i]);
+}	
+}
+
+void banknotes(int value){
+	int bank[] = {100,50,20,10,5,2,1};
+	int used[] = {0,0,0,0,0,0,0}; // 7 notas
+	
+printf("%d\n",value);
+
+while(value != 0){
+	switch(value-use(bank,value)){
+		
+		case 100:
+			used[0]++;
+			value = use(bank,value);
+			break;
+		case 50:
+			used[1]++;
+			value = use(bank,value);
+			break;
+		case 20:
+			used[2]++;
+			value = use(bank,value);
+			break;
+		case 10:
+			used[3]++;
+			value = use(bank,value);
+			break;
+		case 5:
+			used[4]++;
+			value = use(bank,value);
+			break;
+		case 2:
+			used[5]++;
+			value = use(bank,value);
+			break;
+		case 1:
+			used[6]++;	
+			value = use(bank,value);
+			break;
+			
+	}
+		
+
+}
+
+print_bank(used,bank);
+	
+	
+}
+
 int main(){
-
-int tmp,s;
-float spent;
-scanf("%d", &tmp);
-s +=tmp;
-scanf("%d", &tmp);
-s *= tmp;
-spent = (float)s/12;
-printf("%.3f\n",spent);
-
+int num;
+scanf("%d",&num);
+banknotes(num);
 	
 }
