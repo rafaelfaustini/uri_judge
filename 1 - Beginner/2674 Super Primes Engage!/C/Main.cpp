ffb70bbba366d6ac14e443fc9133cbdd1382504a
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-08-18 16:48:05 -0300

    2674

--- 1 - Beginner/2674 Super Primes Engage!/C/Main.cpp
+++ 1 - Beginner/2674 Super Primes Engage!/C/Main.cpp
@@ -1,63 +1,40 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-
   int primo(int n) {
-    int count = 0;
-	int i;
-    for (i = 1; i <= n; i++) {
-      if (n % i == 0)
-        count++;
-    }
-    if (count == 2)
-      return 1;
-    return 0;
+  	int aux;
+ if(n==0 || n==1) return 0;
+    for(aux = 2; aux*aux <= n; aux++)
+        if(n % aux == 0) return 0;
+    return 1;
   }
 
 int super_prime(int n) {
-  int count = 0;
-  char temp[100000];
-  sprintf(temp, "%d", n);
-  int i;
-  if (primo(n) == 1) {
-    for (i = 0; i < strlen(temp); i++) {
-      int number = temp[i] - '0';
-      if (primo(number) == 1)
-        count++;
-
-    }
-    if (count == strlen(temp))
-      return 1;
-  }
-  return 0;
+	while(n>0){
+		if(!primo(n % 10)) return 0;
+		n/= 10;
+	}
+	return 1;
 }
 
-int count(){
-	static int i=0;
-	i++;
-	return i;
-}
-void inicia(){
-	int n;	
-	if(primo(n)==0){
-		printf("Nada\n");
-	}
-	if(super_prime(n)==0){
-		printf("Primo\n");
-	}
-	printf("Super\n");
-}
 int main(void) {
 	int n;
-	int a=1;
-	
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt","w",stdout);
-	while(count()!=10){	
-	scanf("%d",&n);
-	inicia;
+	while(scanf("%d", &n)!=EOF){
+
+
+  if(primo(n)){
+
+   if(super_prime(n)){
+    printf("Super\n");
+   }
+   
+   else{
+    printf("Primo\n");
+   }
+    }
+
+  else{
+   printf("Nada\n");
+  }
+
 }
-	return 0;
 
 }
