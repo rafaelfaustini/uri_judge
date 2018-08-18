ff5cb9fceaead26c49feb6a003794b81f9ca901d
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-08-18 14:50:38 -0300

    1016
    Improved the optimization and fixed presentation error

--- 1 - Beginner/1016 Distance/C/Main.cpp
+++ 1 - Beginner/1016 Distance/C/Main.cpp
@@ -1,14 +1,9 @@
 #include <stdio.h>
-#define V1 90
-#define V2 60
 int main(void){
-int v,t,d;
-v= V1 - V2;
-
+int d;
 scanf("%d",&d);
-t = (60*d)/v;
-printf("%d minutos",t);
-
+d = (60*d)/30;
+printf("%d minutos\n",d);
 }
 
 
