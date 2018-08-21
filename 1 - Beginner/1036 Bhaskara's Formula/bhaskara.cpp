1dab919ced5501d58bc4180b8dbdb6fbe5b329ce
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-08-21 15:36:34 -0300

    Bhaskara Fixes

--- 1 - Beginner/1036 Bhaskara's Formula/bhaskara.cpp
+++ 1 - Beginner/1036 Bhaskara's Formula/bhaskara.cpp
@@ -4,22 +4,20 @@
 
 int main(){
 	
-	float a,b,c;
-	scanf("%f %f %f",&a,&b,&c);
-	float delta = (b*b)-4*a*c;
+	double a,b,c;
+	scanf("%lf %lf %lf",&a,&b,&c);
+	double delta = (b*b)-(4*a*c);
 	
-	if(delta ==0 || a==0){
-	printf("Impossível de calcular\n");	
-	return 0;
+	if(delta < 0 || a==0){
+	printf("Impossivel de calcular\n");	
+} else
+{
+	double r1= ((b*-1)+ sqrt(delta))/(2*a);
+	double r2= ((b*-1)- sqrt(delta))/(2*a);	
+	printf("R1 = %.5lf\n",r1);
+	printf("R2 = %.5lf\n",r2);	
 }
 
-	float x1= ((b*-1)+ sqrt(delta))/(2*a);
-	float x2= ((b*-1)- sqrt(delta))/(2*a);
-	
-	
-	printf("R1= %.5f\n",x1);
-	printf("R2= %.5f\n",x2);			
-
-	
+	return 0;
 	
 }
